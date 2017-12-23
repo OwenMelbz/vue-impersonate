4fce95424d7faa09e952c6f13e69cae2da89b2a0
Owen <owenmelbz@gmail.com>
2017-12-23 17:35:52 +0000

    init

---
+++ VueImpersonateServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OwenMelbz\VueImpersonate;
+
+use Illuminate\Support\ServiceProvider;
+
+class VueImpersonateServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        dd('sdfds');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        dd('dfds');
+    }
+}
